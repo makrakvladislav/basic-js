8156f5895a532f34c970dc479d3fe7b966500d92
Vlad <makrak.vladislav@yandex.ru>
2022-05-02 03:49:17 +0300

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  	console.log(this.arr.length);
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.arr.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position !== 'number' || position <= 0 || position - 1 >= this.arr.length) {
+      this.arr.length = 0;
+      throw new Error ("You can't remove incorrect link!");
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse()
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.arr.map(item => {
+    	return `( ${item} )`;
+    }).join('~~');
+    //let result = this.arr.join('~~');
+    this.arr.length = 0;
+    return result;
   }
 };
 
